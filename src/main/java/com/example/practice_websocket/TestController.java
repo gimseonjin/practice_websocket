d95a26f4895c2d35c175151f57eb62ea00d9a51d
gimseonjin <kimseonjin616@naver.com>
2022-02-26 18:26:11 +0900

    update test controller to return client ip

--- src/main/java/com/example/practice_websocket/TestController.java
+++ src/main/java/com/example/practice_websocket/TestController.java
@@ -3,11 +3,15 @@ package com.example.practice_websocket;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class TestController {
 
     @GetMapping("/")
-    public String test(){
-        return "hello world!";
+    public String test(HttpServletRequest req){
+        String ip = req.getHeader("X-Forwarded-For");
+        if (ip == null) ip = req.getRemoteAddr();
+        return ip;
     }
 }
