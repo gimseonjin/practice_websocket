0f40029cdc01937fce8e9464e1e322d4089dca01
gimseonjin <kimseonjin616@naver.com>
2022-02-27 17:36:04 +0900

    feature : Create Greeting Model
    front와 back 사이에서 데이터를 전해주는 greeting 모델을 만들었습니다.
    
    참고: #2

---
+++ src/main/java/com/example/practice_websocket/model/Greeting.java
@@ -0,0 +1,18 @@
+package com.example.practice_websocket.model;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
